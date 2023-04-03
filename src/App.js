38fcd3211f7fda82c10d02e2fd35960baccfc808
fransschuijt <fp.schuijt@gmail.com>
2023-04-03 14:39:56 +0200

    opdracht af

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Home from './pages/home/Home';
 import Appointments from './pages/appointments/Appointments';
 import Cavities from './pages/cavities/Cavities';
 import Navigation from './components/navigation/Navigation';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Navigation />
-      <Home />
-      <Whitening />
-      <Appointments />
-      <Cavities />
-    </>
-  );
+    return (
+        <>
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/tanden-bleken" element={<Whitening/>}/>
+                <Route path="/afspraken" element={<Appointments/>}/>
+                <Route path="/gaatjes" element={<Cavities/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
